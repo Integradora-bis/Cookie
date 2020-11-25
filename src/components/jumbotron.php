e6f743122670573ecd2adf4954849b425f8c8088
Fanny Barragan <71847955+fannybarr@users.noreply.github.com>
2020-11-25 12:12:42 -0700

    Update jumbotron.php

--- src/components/jumbotron.php
+++ src/components/jumbotron.php
@@ -2,9 +2,10 @@
 echo'
 
 <div class="cuki position-relative overflow-hidden p-3 p-md-5 m-sm-5 text-center rounded shadow p-3 mb-5">
-  <div class=" col-sm-12 p-lg-5 mx-auto my-5">
+  <div class=" col-sm-12 p-lg-5 mx-auto my-4">
     <div class="container">
-      <h1 class="display-4">Who are we?</h1>
+    <img class="chef my-2" src="https://drive.google.com/thumbnail?id=1ljq-pIN0MrXAuUiwbuZ7jvOhlQkksR44"><br>
+      <h1 class="display-4">Who are we?</h1> <br>
       <p class="lead">As students of Information Technologies in UTCH. We were asked in the integrative class to work on the making of a website. In order to achieve this, we gathered a small group of 4 people. It’s a small company that is working on the development of a single project: Cookie.  The enterprise, named as the project, is made up of 4 people. Diana Ramos, Estefania Barragan, Marcos Mediano and Sofia Gonzalez.
                   Everyone committed to a certain task, either programming, designing databases or testing.</p>
     </div>
