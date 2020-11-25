c93dc855269e6d3dccfb0406ff963ec1e1a90ced
sofiaGonzaloUtch <71847414+sofiaGonzaloUtch@users.noreply.github.com>
2020-11-24 18:46:05 -0700

    Update recipe.php

--- src/components/recipe.php
+++ src/components/recipe.php
@@ -1,7 +1,6 @@
 <?php
 function recipe(){
 echo '
-
 <!-- Recipe info -->
 <div class = "container mb-4">
     <br>
@@ -15,9 +14,8 @@ echo '
 <!-- Recipe procedure/ing -->
 
 <div class="row">
-    <div class="col-sm-4.5">
-        <div id="ing" class="card mb-2" style="max-width: 15rem;">
-            <div class="card-header">INGREDIENTS</div>
+    <div class="cuki position-relative overflow-hidden p-3 p-md-2 m-sm-2 text-center rounded shadow p-3 mb-5">
+            <div>INGREDIENTS</div>
             <div class="card-body text-dark">              
                 <ul class="list-group list-group-flush">
                     <li class="list-group-item">•	½	u	green pepper</li>
@@ -26,13 +24,12 @@ echo '
                     <li class="list-group-item">•	2	tbsp olive oil</li>
                     <li class="list-group-item">•	1/2	tsp	salt</li>
                     <li class="list-group-item">•	1	u	chicken breast</li>
-                  </ul>
-            </div>
+                  </ul>         
           </div>
     </div>
     <div class="col-sm-7.5">
-        <div id="procedure" class="card mb-3" style="max-width: 25rem;">
-            <div class="card-header">PROCEDURE</div>
+        <div class="cuki position-relative overflow-hidden p-1 p-md-2 m-sm-2 text-center rounded shadow p-1 mb-1">
+            <div>PROCEDURE</div>
             <div class="card-body text-dark">
               <p class="card-text">ola</p>
     </div>
