cc7a99398911a3eeda5cbdf6f6e28ee8c4bbeb04
Dianard0710 <71846758+Dianard0710@users.noreply.github.com>
2020-11-25 09:07:13 -0700

    Update categories.php

--- src/components/categories.php
+++ src/components/categories.php
@@ -7,8 +7,8 @@
      <img class="chef" src="https://drive.google.com/thumbnail?id=1ljq-pIN0MrXAuUiwbuZ7jvOhlQkksR44"><br>
      <h1 class="display-4 font-weight-normal" style="color: #ac8b67">Welcome to Cookie!</h1> <br>
      <p class="lead font-weight-normal">What do you want to eat today?</p> <br>
-     <div style="align-items: center;"><form class="form-inline col-sm-6 p-lg-5 mx-auto my-5"></div>
-         <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+     <div style="align-items: center;"><form class="form-inline col-md-3 p-lg-1 mx-auto my-1"></div>
+         <input class="form-control col-md-4 p-lg-1 mx-auto my-1 mr-md-6" type="search" placeholder="Search" aria-label="Search">
          <button class="btn btn-outline-dark my-5 my-sm-0" type="submit">Search</button>
      </form>
  </div>
