c2d4e1526994b12fca1fa3bfb37b7ddabfd43247
Dianard0710 <71846758+Dianard0710@users.noreply.github.com>
2020-11-25 10:32:51 -0700

    Update recipe.php

--- src/components/recipe.php
+++ src/components/recipe.php
@@ -10,9 +10,7 @@ echo '
             <li class="list-inline-item">Individual</li>
             <li class="list-inline-item">-1h</li>
         </ul>
-
 <!-- Recipe procedure/ing -->
-
 <div class="row">
     <div class="cuki position-relative overflow-hidden p-3 p-md-2 m-sm-2 text-center rounded shadow p-3 mb-5">
             <div>INGREDIENTS</div>
@@ -25,15 +23,25 @@ echo '
                     <li class="list-group-item">•	1/2	tsp	salt</li>
                     <li class="list-group-item">•	1	u	chicken breast</li>
                   </ul>         
-          </div>
+            </div>
     </div>
-    <div class="col-sm-7.5">
+    <div class="col-md-5">
         <div class="cuki position-relative overflow-hidden p-1 p-md-2 m-sm-2 text-center rounded shadow p-1 mb-1">
             <div>PROCEDURE</div>
             <div class="card-body text-dark">
               <p class="card-text">ola</p>
+            </div>
+        </div>
+    </div>
+
+    <div class="col-md-4">
+        <div class="embed-responsive position-relative  text-center">
+        <img src="https://drive.google.com/thumbnail?id=1klEW8Y4jKtNmbGTMlWqoW1_hyEmgHHeh" class="img-fluid rounded shadow" alt="Honey lime chicken style="width:200% height:300%" >
+        </div>
     </div>
 </div>
+
+
 ';
 }
 ?>
