308e27d3ae635509e2fbf391726874eacc71f8f2
sofiaGonzaloUtch <71847414+sofiaGonzaloUtch@users.noreply.github.com>
2020-11-27 21:22:29 -0700

    Update recipe.php

--- src/components/recipe.php
+++ src/components/recipe.php
@@ -10,38 +10,44 @@ echo '
             <li class="list-inline-item">Individual</li>
             <li class="list-inline-item">-1h</li>
         </ul>
-<!-- Recipe procedure/ing -->
-<div class="row">
-    <div class="cuki position-relative overflow-hidden p-3 p-md-2 m-sm-2 text-center rounded shadow p-3 mb-5">
-            <div>INGREDIENTS</div>
-            <div class="card-body text-dark">              
-                <ul class="list-group list-group-flush">
-                    <li class="list-group-item">•	½	u	green pepper</li>
-                    <li class="list-group-item">•	½	u	red pepper</li>
-                    <li class="list-group-item">•	¼	u onion</li>
-                    <li class="list-group-item">•	2	tbsp olive oil</li>
-                    <li class="list-group-item">•	1/2	tsp	salt</li>
-                    <li class="list-group-item">•	1	u	chicken breast</li>
-                  </ul>         
-            </div>
-    </div>
-    <div class="col-md-5">
-        <div class="cuki position-relative overflow-hidden p-1 p-md-2 m-sm-2 text-center rounded shadow p-1 mb-1">
-            <div>PROCEDURE</div>
-            <div class="card-body text-dark">
-              <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad libero placeat ut facilis distinctio, earum inventore architecto neque nemo. Voluptates odio tempore voluptatibus, repellendus incidunt fugiat ipsum ratione aliquid unde.Aspernatur unde corrupti, illum facere autem deserunt repellat dolore. Molestias cumque, porro consequuntur atque praesentium ad eum. Aliquid neque consectetur, dolorem itaque aliquam obcaecati tenetur reprehenderit optio, provident fugiat ipsam?</p>
-            </div>
-        </div>
-    </div>
 
-    <div class="col-md-4">
-        <div class="embed-responsive position-relative  text-center">
-        <img src="https://drive.google.com/thumbnail?id=1klEW8Y4jKtNmbGTMlWqoW1_hyEmgHHeh" class="img-fluid rounded shadow" alt="Honey Lime Chicken" style="width: 70%; height: auto;">
+<!-- Ingredients -->
+    <div class="row">
+        <div class="cuki position-relative overflow-hidden p-3 p-md-2 m-sm-2 text-center rounded shadow p-3 mb-5" id="white-bg">
+                <div>INGREDIENTS</div>
+                <div class="card-body text-dark">              
+                    <ul class="list-group list-group-flush">
+                        <li class="list-group-item">•	½	u	green pepper</li>
+                        <li class="list-group-item">•	½	u	red pepper</li>
+                        <li class="list-group-item">•	¼	u onion</li>
+                        <li class="list-group-item">•	2	tbsp olive oil</li>
+                        <li class="list-group-item">•	1/2	tsp	salt</li>
+                        <li class="list-group-item">•	1	u	chicken breast</li>
+                    </ul>         
+                </div>
         </div>
+
+        <!-- Procedure -->
+        <div class="col-md-5">
+            <div class="cuki position-relative overflow-hidden p-1 p-md-2 m-sm-2 text-center rounded shadow p-1 mb-1" id="white-bg">
+                <div>PROCEDURE</div>
+                    <div class="card-body text-dark">
+                        <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad libero placeat ut facilis distinctio, earum inventore architecto neque nemo. Voluptates odio tempore voluptatibus, repellendus incidunt fugiat ipsum ratione aliquid unde.Aspernatur unde corrupti, illum facere autem deserunt repellat dolore. Molestias cumque, porro consequuntur atque praesentium ad eum. Aliquid neque consectetur, dolorem itaque aliquam obcaecati tenetur reprehenderit optio, provident fugiat ipsam?</p>
+                    </div>
+            </div>
+        </div>
+
+        <!-- Image -->
+        <div class="col-md-4">
+            <div class="embed-responsive position-relative  text-center">
+                <img src="https://drive.google.com/thumbnail?id=1klEW8Y4jKtNmbGTMlWqoW1_hyEmgHHeh" class="img-fluid rounded shadow" alt="Honey Lime Chicken" style="width: 80%; height: auto;">
+            </div>
+        </div>
+    
     </div>
-   
 </div>
-</div>
+
+<br><br>
 ';
 }
 ?>
